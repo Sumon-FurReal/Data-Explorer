0a1c6a588e95b9a1f2328162a0c3ef6bde88b51b
Sumon-FurReal <114070757+Sumon-FurReal@users.noreply.github.com>
2023-07-31 12:37:09 +0530

    Update DataExplorer.py

--- DataExplorer.py
+++ DataExplorer.py
@@ -64,7 +64,7 @@ if data is not None:
 
 # Histogram
     elif plot_type == "Hist":
-        fig = px.histogram(data, x=x_axis ,  color_discrete_sequence=px.colors.sequential.Pastel)
+        fig = px.histogram(data, x=x_axis ,  color_discrete_sequence=px.colors.sequential.Glasbey)
         st.plotly_chart(fig)
 
 # Box Plot
