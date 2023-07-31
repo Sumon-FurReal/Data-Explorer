d0f1c90ada2173d3a2e5bee349e76409cfeda621
Sumon-FurReal <114070757+Sumon-FurReal@users.noreply.github.com>
2023-07-31 18:34:29 +0530

    Update DataExplorer.py

--- DataExplorer.py
+++ DataExplorer.py
@@ -49,27 +49,37 @@ if data is not None:
 # Scatter plot
     if plot_type == "Scatter":
         fig = px.scatter(data, x=x_axis, y=y_axis, color_discrete_sequence=px.colors.qualitative.Light24)
+        fig.update_xaxes(showgrid=True, gridwidth=1.0)
+        fig.update_yaxes(showgrid=True, gridwidth=1.0)
         st.plotly_chart(fig)
 
 
 # Line chart
     elif plot_type == "Line":
         fig = px.line(data, x=x_axis, y=y_axis, color_discrete_sequence=px.colors.qualitative.Pastel)
+        fig.update_xaxes(showgrid=True, gridwidth=1.0)
+        fig.update_yaxes(showgrid=True, gridwidth=1.0)
         st.plotly_chart(fig)
 
 # Bar chart
     elif plot_type == "Bar":
         fig = px.bar(data, x=x_axis, y=y_axis, color_discrete_sequence=px.colors.qualitative.Pastel)
+        fig.update_xaxes(showgrid=True, gridwidth=1.0)
+        fig.update_yaxes(showgrid=True, gridwidth=1.0)
         st.plotly_chart(fig)
 
 # Histogram
     elif plot_type == "Hist":
         fig = px.histogram(data, x=x_axis ,  color_discrete_sequence=px.colors.sequential.Viridis)
+        fig.update_xaxes(showgrid=True, gridwidth=1.0)
+        fig.update_yaxes(showgrid=True, gridwidth=1.0)
         st.plotly_chart(fig)
 
 # Box Plot
     elif plot_type == "Box":
         fig = px.box(data, x=x_axis, y=y_axis, color_discrete_sequence=px.colors.sequential.YlGnBu)
+        fig.update_xaxes(showgrid=True, gridwidth=1.0)
+        fig.update_yaxes(showgrid=True, gridwidth=1.0)
         st.plotly_chart(fig)
 
 
