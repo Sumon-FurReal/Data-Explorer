67c3dd85792079a39dfb41a00b1032b4f0fe2327
Sumon-FurReal <114070757+Sumon-FurReal@users.noreply.github.com>
2023-07-31 18:42:29 +0530

    DataExplorer.py

--- DataExplorer.py
+++ DataExplorer.py
@@ -49,8 +49,8 @@ if data is not None:
 # Scatter plot
     if plot_type == "Scatter":
         fig = px.scatter(data, x=x_axis, y=y_axis, color_discrete_sequence=px.colors.qualitative.Safe)
-        fig.update_xaxes(showgrid=False)
-        fig.update_yaxes(showgrid=False)
+        fig.update_xaxes(showgrid=False,  gridwidth=0.5)
+        fig.update_yaxes(showgrid=False,  gridwidth=0.5)
         st.plotly_chart(fig)
 
 
@@ -70,7 +70,7 @@ if data is not None:
 
 # Histogram
     elif plot_type == "Hist":
-        fig = px.histogram(data, x=x_axis ,  color_discrete_sequence=px.colors.sequential.Magma)
+        fig = px.histogram(data, x=x_axis ,  color_discrete_sequence=px.colors.sequential.Safe)
         fig.update_xaxes(showgrid=True, gridwidth=0.5)
         fig.update_yaxes(showgrid=True, gridwidth=0.5)
         st.plotly_chart(fig)
