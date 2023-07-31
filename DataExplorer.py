4a79fee23fffe70f2330c882bd3381b4ad0aaff4
Sumon-FurReal <114070757+Sumon-FurReal@users.noreply.github.com>
2023-07-31 18:56:35 +0530

    DataExplorer.py

--- DataExplorer.py
+++ DataExplorer.py
@@ -49,8 +49,8 @@ if data is not None:
 # Scatter plot
     if plot_type == "Scatter":
         fig = px.scatter(data, x=x_axis, y=y_axis, color_discrete_sequence=px.colors.qualitative.Safe)
-        fig.update_xaxes(showgrid=True, gridwidth=1.0)
-        fig.update_yaxes(showgrid=True, gridwidth=1.0)
+        fig.update_xaxes(showgrid=True, gridwidth=0.3)
+        fig.update_yaxes(showgrid=True, gridwidth=0.3)
         st.plotly_chart(fig)
 
 
