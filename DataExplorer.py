e75bd66b37acbb1e4c9fe12e384226369402101e
Sumon-FurReal <114070757+Sumon-FurReal@users.noreply.github.com>
2023-07-31 18:53:34 +0530

    DataExplorer.py

--- DataExplorer.py
+++ DataExplorer.py
@@ -70,7 +70,7 @@ if data is not None:
 
 # Histogram
     elif plot_type == "Hist":
-        fig = px.histogram(data, x=x_axis ,  color_discrete_sequence=px.colors.sequential.Alphabet)
+        fig = px.histogram(data, x=x_axis ,  color_discrete_sequence=px.colors.qualitative.Set3)
         fig.update_xaxes(showgrid=False)
         fig.update_yaxes(showgrid=False)
         st.plotly_chart(fig)
